b3dc425470c82dd59919d70024a82a4c53ab1dbf
deam <deamme@users.noreply.github.com>
2019-11-11 18:22:03 +0100

    Mint 5000 instead of 200

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ class App extends Component {
   }
 
   mint(token) {
-    const amount = BigNumber(200000000000000000000);
-    console.log("Requesting 200 " + token);
+    const amount = BigNumber(5000000000000000000000);
+    console.log("Requesting 5000 " + token);
 
     if (token === "DAI") {
       this.state.factory.mint(DAI, this.state.account, amount, {
